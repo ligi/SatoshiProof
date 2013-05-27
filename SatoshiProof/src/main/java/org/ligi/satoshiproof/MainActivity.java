89cbfaeb2eff49745a3b84cb9657f0a3341734d3
ligi <ligi@ligi.de>
2013-05-27 22:03:57 +0200

    initial

---
+++ SatoshiProof/src/main/java/org/ligi/satoshiproof/MainActivity.java
@@ -0,0 +1,106 @@
+package org.ligi.satoshiproof;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Handler;
+import android.provider.MediaStore;
+
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import org.apache.commons.io.FileUtils;
+import org.ligi.androidhelper.helpers.dialog.DialogDiscardingOnClickListener;
+
+import java.io.*;
+
+/**
+ * Created by ligi on 5/26/13.
+ */
+public class MainActivity extends Activity {
+
+    private static final int ACTIVITY_SELECT_IMAGE = 1;
+    private TextView textView;
+    private Handler handler;
+    private ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handler = new Handler();
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textToProof);
+        setupButtons();
+    }
+
+    private void setupButtons() {
+        findViewById(R.id.proofTextButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ProofAsyncTask(MainActivity.this, textView.getText().toString().getBytes()).execute();
+            }
+        });
+
+        findViewById(R.id.pickPictureButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, ACTIVITY_SELECT_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    final Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch (requestCode) {
+            case ACTIVITY_SELECT_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    progressDialog = new ProgressDialog(this);
+                    progressDialog.setMessage("Fetching file");
+                    progressDialog.show();
+                    final ImageFromIntentUriExtractor imageExtractor = new ImageFromIntentUriExtractor(MainActivity.this);
+                    new Thread(new Runnable() {
+
+                        @Override
+                        public void run() {
+
+                            File bitmapFile = imageExtractor.extract(imageReturnedIntent.getData());
+                            proofFile(bitmapFile );
+
+                        }
+                    }).start();
+                }
+        }
+    }
+
+    private void proofFile(final File file) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    byte[] imageBytes = FileUtils.readFileToByteArray(file);
+                    new ProofAsyncTask(MainActivity.this, imageBytes).execute();
+                    progressDialog.dismiss();
+                } catch (IOException e) {
+                    new AlertDialog.Builder(MainActivity.this).setMessage("Could not open " + file + " " + e)
+                            .setPositiveButton("OK", new DialogDiscardingOnClickListener()).show();
+
+                }
+
+            }
+        });
+
+    }
+
+
+}
---
+++ SatoshiProof/src/main/java/org/ligi/satoshiproof/ProofAsyncTask.java
@@ -0,0 +1,83 @@
+package org.ligi.satoshiproof;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.util.Log;
+import com.google.bitcoin.core.Address;
+import com.google.bitcoin.core.NetworkParameters;
+import com.google.bitcoin.core.Utils;
+import de.schildbach.wallet.integration.android.BitcoinIntegration;
+import org.ligi.androidhelper.AndroidHelper;
+import org.ligi.androidhelper.helpers.dialog.DialogDiscardingOnClickListener;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+* Created by ligi on 5/26/13.
+*/
+class ProofAsyncTask extends AsyncTask<Void, String, String> {
+
+    private ProgressDialog progressDialog;
+    private Address address;
+    private final byte[] data;
+    private final Context context;
+
+
+    public ProofAsyncTask(Context context, byte[] data) {
+        this.data = data;
+        this.context = context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        progressDialog=new ProgressDialog(context);
+        progressDialog.setMessage("Checking this text");
+        progressDialog.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(String... values) {
+        progressDialog.setMessage(values[0]);
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        address=new Address( NetworkParameters.prodNet(), Utils.sha256hash160(data));
+        publishProgress("searching for Address: " + address.toString());
+
+        try {
+            URL url = new URL("http://blockexplorer.com/q/addressfirstseen/"+address.toString());
+            return AndroidHelper.at(url).downloadToString();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        progressDialog.dismiss();
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setPositiveButton("OK",new DialogDiscardingOnClickListener());
+        if (s.toLowerCase().startsWith("never seen")) {
+            alertBuilder.setMessage("The existence of this is not proven yet.");
+            alertBuilder.setNeutralButton("Add Proof",new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    BitcoinIntegration.request(context, address.toString(), 1);
+                }
+            });
+
+        } else {
+            alertBuilder.setMessage("The existence of this was proven on:" + s);
+        }
+        alertBuilder.show();
+        super.onPostExecute(s);
+    }
+}
