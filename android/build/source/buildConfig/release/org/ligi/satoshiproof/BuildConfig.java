a1e3cab1fde78d16d9091b08af5a2d2119ae0286
ligi <ligi@ligi.de>
2014-01-01 23:39:35 +0100

    bump

--- android/build/source/buildConfig/release/org/ligi/satoshiproof/BuildConfig.java
+++ android/build/source/buildConfig/release/org/ligi/satoshiproof/BuildConfig.java
@@ -1,7 +1,12 @@
-/** Automatically generated file. DO NOT MODIFY */
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
 package org.ligi.satoshiproof;
 
 public final class BuildConfig {
-    public static final boolean DEBUG = false;
-
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "org.ligi.satoshiproof";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 4;
 }
--- android/build/source/r/release/org/ligi/satoshiproof/R.java
+++ android/build/source/r/release/org/ligi/satoshiproof/R.java
@@ -11,9 +11,6 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        /**  From: file:/home/ligi/git/SatoshiProof/android/src/main/res/values/dimens.xml 
- From: file:/home/ligi/git/SatoshiProof/android/src/main/res/values-sw720dp-land/dimens.xml 
-         */
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
         public static final int help_padding=0x7f050002;
@@ -44,21 +41,16 @@ public final class R {
         public static final int help=0x7f040000;
     }
     public static final class string {
-        /**  From: file:/home/ligi/git/SatoshiProof/android/src/main/res/values/strings.xml 
-         */
         public static final int action_help=0x7f060000;
         public static final int app_name=0x7f060001;
     }
     public static final class style {
-        /**  From: file:/home/ligi/git/SatoshiProof/android/src/main/res/values/styles.xml 
-
+        /** 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
- From: file:/home/ligi/git/SatoshiProof/android/src/main/res/values-v11/styles.xml 
  API 11 theme customizations can go here. 
- From: file:/home/ligi/git/SatoshiProof/android/src/main/res/values-v14/styles.xml 
  API 14 theme customizations can go here. 
          */
         public static final int AppBaseTheme=0x7f070000;
