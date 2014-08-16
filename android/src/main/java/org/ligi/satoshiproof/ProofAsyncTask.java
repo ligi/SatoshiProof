259dfae4e0aa670229fdcb4e57aadd80f2668488
ligi <ligi@ligi.de>
2014-08-16 19:58:30 +0200

    nice up code - use okhttp & prevent FC on network problems

--- android/src/main/java/org/ligi/satoshiproof/ProofAsyncTask.java
+++ android/src/main/java/org/ligi/satoshiproof/ProofAsyncTask.java
@@ -1,20 +1,20 @@
 package org.ligi.satoshiproof;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 
 import com.google.bitcoin.core.Address;
 import com.google.bitcoin.core.Utils;
 import com.google.bitcoin.params.MainNetParams;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
-import org.ligi.axt.AXT;
 import org.ligi.axt.listeners.DialogDiscardingOnClickListener;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -25,20 +25,20 @@ import de.schildbach.wallet.integration.android.BitcoinIntegration;
 
 class ProofAsyncTask extends AsyncTask<Void, String, String> {
 
-    private ProgressDialog progressDialog;
     private Address address;
     private final byte[] data;
-    private final Context context;
+    private final Activity activity;
+    private final ProgressDialog progressDialog;
 
-    public ProofAsyncTask(Context context, byte[] data) {
+    public ProofAsyncTask(Activity activity, byte[] data) {
         this.data = data;
-        this.context = context;
+        this.activity = activity;
+        progressDialog = new ProgressDialog(activity);
     }
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
-        progressDialog = new ProgressDialog(context);
         progressDialog.setMessage("Checking this text");
         progressDialog.show();
     }
@@ -55,9 +55,18 @@ class ProofAsyncTask extends AsyncTask<Void, String, String> {
         publishProgress("searching for Address: " + address.toString());
 
         try {
-            final URL url = new URL("http://blockexplorer.com/q/addressfirstseen/" + address.toString());
-            return AXT.at(url).downloadToString();
-        } catch (MalformedURLException e) {
+
+            final OkHttpClient client = new OkHttpClient();
+
+            final Request request = new Request.Builder()
+                    .url("http://blockexplorer.com/q/addressfirstseen/" + address.toString())
+                    .build();
+
+            final Response response = client.newCall(request).execute();
+            return response.body().string();
+
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
@@ -65,15 +74,29 @@ class ProofAsyncTask extends AsyncTask<Void, String, String> {
 
     @Override
     protected void onPostExecute(String firstSeenDateString) {
+
+        if (activity.isFinishing()) {
+            return;
+        }
+
         progressDialog.dismiss();
-        final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+
+        if(firstSeenDateString==null) {
+            new AlertDialog.Builder(activity)
+                    .setMessage("there where network problems - please try again later")
+                    .setPositiveButton(android.R.string.ok,null)
+                    .show();
+            return;
+        }
+
+        final AlertDialog.Builder alertBuilder = new AlertDialog.Builder(activity);
         alertBuilder.setPositiveButton("OK", new DialogDiscardingOnClickListener());
         if (firstSeenDateString.toLowerCase(Locale.getDefault()).startsWith("never seen")) {
             alertBuilder.setMessage("The existence of this is not proven yet.");
             alertBuilder.setNeutralButton("Add Proof", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
-                    BitcoinIntegration.request(context, address.toString(), 1);
+                    BitcoinIntegration.request(activity, address.toString(), 1);
                 }
             });
 
