aee5a19d5a85e2064ab8a9940f8173cc90ecce82
ligi <ligi@ligi.de>
2014-08-19 14:08:22 +0200

    fix a FC when there is a network problem

--- android/src/main/java/org/ligi/satoshiproof/LastHashActivity.java
+++ android/src/main/java/org/ligi/satoshiproof/LastHashActivity.java
@@ -2,6 +2,8 @@ package org.ligi.satoshiproof;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.view.MenuItem;
 import com.androidquery.AQuery;
 
 import org.ligi.axt.AXT;
+import org.ligi.axt.listeners.ActivityFinishingOnClickListener;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -61,6 +64,13 @@ public class LastHashActivity extends Activity {
 
         @Override
         protected void onPostExecute(String s) {
+            if (s==null) {
+                new AlertDialog.Builder(LastHashActivity.this)
+                        .setMessage("Could not connect to network - please try again later.")
+                        .setPositiveButton(android.R.string.ok,new ActivityFinishingOnClickListener(LastHashActivity.this))
+                        .show();
+                return;
+            }
             final String url = "http://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=" + s.replace("\n", "");
             final AQuery hashImage = aQuery.id(R.id.hash_image);
             hashImage.visible();
