21043dcd22eedfce24e3919bd33218eda3c4f83e
ligi <ligi@ligi.de>
2016-10-14 13:27:14 +0200

    F

--- android/src/androidTest/kotlin/TheMainActivity.kt
+++ android/src/androidTest/kotlin/TheMainActivity.kt
@@ -4,7 +4,9 @@ import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
+import android.view.WindowManager
 import com.jraska.falcon.FalconSpoon
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.ligi.satoshiproof.MainActivity
@@ -15,23 +17,30 @@ class TheMainActivity {
     @get:Rule
     var activityActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
+    val activity by lazy { activityActivityTestRule.activity }
+
+    @Before
+    fun start() {
+        activity.runOnUiThread { activity.window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD) }
+    }
+
     @Test
     fun activityLaunches() {
-        FalconSpoon.screenshot(activityActivityTestRule.activity,"main_activity")
+        FalconSpoon.screenshot(activity,"main_activity")
     }
 
     @Test
     fun helpShows() {
         onView(withId(R.id.action_help)).perform(click())
         onView(withText(R.string.help_title)).check(matches(isDisplayed()))
-        FalconSpoon.screenshot(activityActivityTestRule.activity,"help")
+        FalconSpoon.screenshot(activity,"help")
     }
 
     @Test
     fun lastHashShows() {
         onView(withId(R.id.action_hash)).perform(click())
         onView(withId(R.id.hash_text)).check(matches(isDisplayed()))
-        FalconSpoon.screenshot(activityActivityTestRule.activity,"last_hash")
+        FalconSpoon.screenshot(activity,"last_hash")
     }
 
 }
